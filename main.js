5fd9e0692a97cf390cc3437d4ef9d8094f279868
Dakshika <dakshika001@tamu.edu>
2019-12-15 18:07:52 -0800

    updating form

--- main.js
+++ main.js
@@ -26,6 +26,46 @@ $(document).ready(function()
         }, 2000)
     });
 
+    $('.submit').click(function(event) {
+        
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var service = $('.service').val()
+        var subject = $('.subject').val()
+        var statusElm = $('.status')
+        statusElm.emplty()
+
+        if(email.length > 5 && email.includes('@') && email.includes('.'))
+        {
+            statusElm.append('<div>Email is valid</div>')
+        }
+        else
+        {
+            event.preventDefault()
+            statusElm.append('<div> Email is not valid</div>')
+        }
+
+        if(subject.length > 10)
+        {
+            statusElm.append('<div>Subject is valid</div>')
+        }
+        else
+        {
+            event.preventDefault()
+            statusElm.append('<div> Subject is not valid</div>')
+        }
+
+        if(name.length > 2)
+        {
+            statusElm.append('<div>Name is valid</div>')
+        }
+        else
+        {
+            event.preventDefault()
+            statusElm.append('<div> Name is not valid</div>')
+        }
+    })
+
     AOS.init({
         easing: 'ease',
         duration: 1800,
